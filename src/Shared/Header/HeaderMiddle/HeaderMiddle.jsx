2e85922465b92cd8512b75008c7d212613f1bb50
unknown <fozlurrahman9933@gmail.com>
2023-12-16 15:56:16 +0600

    add the header middle bar

--- src/Shared/Header/HeaderMiddle/HeaderMiddle.jsx
+++ src/Shared/Header/HeaderMiddle/HeaderMiddle.jsx
@@ -1,9 +1,35 @@
+import logo from '../../../assets/images/logo/Logo.png';
+import { BiPhoneCall } from "react-icons/bi";
 
 
 const HeaderMiddle = () => {
     return (
-        <div>
-            <h1>Header middle bar</h1>
+        // header middle bar jsx markup
+        <div className='flex justify-between py-5'>
+
+            {/* site logo */}
+            <div>
+                <img src={logo} alt="" />
+            </div>
+
+            {/* header middle search area */}
+            <div>
+                <form className='flex' action="">
+                    <input className='border px-4 py-2 lg:w-full rounded-s outline-none' type="text" placeholder='Search' />
+                    <button className='bg-[var(--high-light)] text-white py-2 px-4 border border-[var(--high-light)] rounded-e'>search</button>
+                </form>
+            </div>
+
+            {/* header middle contact info */}
+            <div className='flex items-center space-x-2'>
+                <div>
+                    <BiPhoneCall className='text-4xl' />
+                </div>
+                <div>
+                    <p>Customers services</p>
+                    <h4 className='text-xl text-black'>(219) 555-0114</h4>
+                </div>
+            </div>
         </div>
     );
 };
