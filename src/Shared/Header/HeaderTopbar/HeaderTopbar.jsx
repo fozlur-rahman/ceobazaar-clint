cee6327ecf6db78c010e84f2450d0d195b174adb
unknown <fozlurrahman9933@gmail.com>
2023-12-16 15:29:04 +0600

    add the header top bar

--- src/Shared/Header/HeaderTopbar/HeaderTopbar.jsx
+++ src/Shared/Header/HeaderTopbar/HeaderTopbar.jsx
@@ -1,9 +1,29 @@
+import { GoLocation } from "react-icons/go";
 
 
 const HeaderTopbar = () => {
     return (
-        <div>
-            <h1 className="text-red-500">Header top bar</h1>
+        //header topbar jsx markup
+        <div className="h-12 flex justify-between border-b">
+
+            {/* location */}
+            <p className="flex items-center space-x-2">
+                <GoLocation /> <span>Store Location: Lincoln- 344, Illinois, Chicago, USA</span>
+            </p>
+
+            {/* select language and currency option form */}
+            <form className="flex space-x-2">
+                <select className="outline-none  bg-transparent border-none text-gray-500" name="" id="">
+                    <option value="english">English</option>
+                    <option value="bangla">Bangla</option>
+                </select>
+
+                <select className="outline-none  bg-transparent border-none text-gray-500" name="" id="">
+                    <option value="usd">USD</option>
+                    <option value="bdt">BDT</option>
+                </select>
+            </form>
+
         </div>
     );
 };
